437845595b30a90176a24c6bb253ff234e6fd832
RNT13 <renatornt13@gmail.com>
2025-10-16 19:23:45 -0300

    Update: - adição da parte do admin

--- core/urls.py
+++ core/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 # Em core/urls.py
 
-from blog import views
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
 ]
+
 
 
 
